3fe7f0a4135e087b7946446d5ff0df8a205b2e5a
Alex Booker <booker@booker.codes>
2023-03-14 22:26:33 +0000

    Add scroll buttons

--- js/gallery.js
+++ js/gallery.js
@@ -7,16 +7,58 @@ console.log(gallery_scroller);
 const gallery_item_size = gallery_scroller.querySelector('img').clientWidth;
 // console.log(gallery_item_size);
 
-gallery.querySelector('.btn.next').addEventListener('click', scrollToNextPage);
-gallery.querySelector('.btn.prev').addEventListener('click', scrollToPrevPage);
+const nxt = gallery.querySelector('.btn.next')
+nxt.addEventListener('click', scrollToNextPage);
+const prev = gallery.querySelector('.btn.prev')
+prev.addEventListener('click', scrollToPrevPage);
+gallery_scroller.addEventListener('scroll', e => {
+    if (gallery_scroller.scrollLeft > 0) {
+        prev.style.display = "block"
+    } else {
+        prev.style.display = "none"
+    }
+
+    const scrollable = gallery_scroller.scrollWidth  - gallery_scroller.offsetWidth
+    if (gallery_scroller.scrollLeft >= scrollable) {
+        nxt.style.display = "none"
+    } else {
+        nxt.style.display = "block"
+    }
+
+    // 985
+    
+    // console.log("scrollable", scrollable);
+    // console.log("scrollWidth", gallery_scroller.scrollWidth);
+    // console.log("offsetWidth", gallery_scroller.offsetWidth);
+    // console.log("clientWidth", gallery_scroller.clientWidth);
+    // console.log("scrollLeft", gallery_scroller.scrollLeft);
+
+    // if scrollLeft > 0 then show next btn
+    // console.log(gallery_scroller.scrollLeft + '/' + (gallery_scroller.offsetWidth - gallery_scroller.clientWidth));
+    // console.log(gallery_scroller.offsetWidth);
+});
 
 // // For paginated scrolling, simply scroll the gallery one item in the given
 // // direction and let css scroll snaping handle the specific alignment.
 function scrollToNextPage() {
-  gallery_scroller.scrollBy(gallery_item_size, 0);
+  gallery_scroller.scrollBy({
+    left: gallery_item_size,
+    top: 0,
+    behavior: 'smooth'
+  });
+//   updateBtns()
 }
 function scrollToPrevPage() {
-  gallery_scroller.scrollBy(-gallery_item_size, 0);
+  gallery_scroller.scrollBy({
+    left: -gallery_item_size,
+    top: 0,
+    behavior: 'smooth'
+  });
+//   updateBtns()
+}
+
+function updateBtns () {
+    console.log("scrolled one way or another");
 }
 
 // function updateAlignment(event) {
